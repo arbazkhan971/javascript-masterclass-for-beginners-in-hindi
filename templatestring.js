7886e92c761fd8b35995dbf36d827469f5b0a2ef
arbaz <arbazkhan971@gmail.com>
2022-06-06 00:59:03 +0530

    template string is added

---
+++ templatestring.js
@@ -0,0 +1,46 @@
+/***
+
+    Template literals (template strings) allow you to use strings or embedded expressions 
+    in the form of a string. 
+    They are enclosed in backticks ``. For example,
+
+    const name = 'Jack';
+    console.log(`Hello ${name}!`); // Hello Jack!
+
+***/
+
+// Template Literals for Strings
+// const str = 'this is string with 'quotes''; // invalid
+const str1 = 'this is string with \'quotes\'';
+const str3 = 'this is string with "quotes"';
+const str4 = "this is string with 'quotes'";
+// const str5 = "this is string with "quotes""; // invalid
+
+const str6 = `this is string with 'quotes'`;
+const str7 = `this is string with "quotes"`;
+console.log(str6);
+console.log(str7);
+
+
+// Multiline Strings Using Template Literals
+const str8 = "this is multilien string span across\n" +
+"multiple lines";
+console.log(str8);
+
+const str9 = `this is multiline string 
+spanned across 
+multiple lines
+int he code`;
+console.log("str9",str9);
+
+// Expression Interpolation
+const first_name ='Jack';
+const last_name = 'Smith';
+// console.log("hello"+" "+first_name+" "+last_name);
+
+// console.log("via template string",`hello ${first_name} ${last_name}`);
+const pie = 3.14;
+console.log(`the pie is ${pie}`);
+const radius = 20;
+console.log(`the radius is ${radius}`);
+console.log(`the area of circle is ${pie*radius*radius}`);
