79ec9c8cd83e124e26cc897418483d18d11b03a3
arbaz <arbazkhan971@gmail.com>
2022-06-06 22:43:44 +0530

    added changes into files

---
+++ nullvsundefined.js
@@ -0,0 +1,20 @@
+/*** 
+ 
+Definition:
+    Undefined: It means the value does not exist in the compiler.
+    Null:It is the intentional absence of the value. 
+        It is one of the primitive values of JavaScript. 
+
+***/
+
+// Undefined 
+// let favNum;
+// console.log(favNum,favNum+3,`My favorite number is ${favNum}`);
+
+
+// Null 
+// let favNum = null;
+// console.log(favNum,favNum+3,`My favorite number is ${favNum}`);
+// Null Vs Undefined
+console.log( null == undefined);
+console.log( null === undefined);
