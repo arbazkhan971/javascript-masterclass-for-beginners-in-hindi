3f75665952d5d7f5aef49e3f987ac0ba6e72998d
arbaz <arbazkhan971@gmail.com>
2023-05-28 13:40:38 +0530

    added function

--- dom.js
+++ dom.js
@@ -1,47 +1,47 @@
-console.log('dom.js')
-// get element by id
-console.log(document)
-const h1dom = document.getElementById('dom')
-console.log(h1dom)
-console.log(h1dom.textContent)
-console.log('------------------')
+const anotherh1tag = document.createElement('h1');
+anotherh1tag.textContent = 'I am a new h1 tag';
+console.log(anotherh1tag);
 
-const domid1 = document.getElementById('domid1')
-console.log(domid1)
-console.log(domid1.textContent)
-// get element by class
-const class1 = document.getElementsByClassName('dom1class')
-console.log(class1)
-console.log(typeof class1)
-console.log(class1)
-// console.log("class1",class1.textContent)
+document.body.appendChild(anotherh1tag);
+console.log("document.body", document.body.innerHTML);
 
-const classdom2 = document.getElementsByClassName('dom2')[0]
-console.log(classdom2)
-console.log(classdom2.textContent)
-// get element by tag
-const tag1 = document.getElementsByTagName('p')[3]
-console.log(tag1.textContent)
+const addptag = document.createElement('p');
+addptag.textContent = 'I am a new p tag';
+console.log(addptag);
+document.body.appendChild(addptag);
 
-// get element by selector
-const selector1 = document.querySelector('#dom')
-console.log("forid",selector1.textContent)
-const selector2 = document.querySelector('.dom1class')
-console.log("forid",selector2.textContent)
-const selector3 = document.querySelector('p')
-console.log("p",selector3.textContent)
+const adda = document.createElement('a');
+adda.textContent = 'I am a new a tag';
+adda.href = 'https://www.google.com';
+console.log(adda);
+document.body.appendChild(adda);
 
-// get element by selector all
-const selectorall1 = document.querySelectorAll('#dom')
-console.log("forid",selectorall1)
-const selectorall2 = document.querySelectorAll('.dom1class')
-console.log("forid",selectorall2)
-const selectorall3 = document.querySelectorAll('p')
-console.log("p",selectorall3)
+const addimg = document.createElement('img');
+addimg.src = 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png';
+console.log(addimg);
+document.body.appendChild(addimg);
 
-// get element by attribute
-// get element by attribute value
-// get element by attribute name
-// get element by attribute name and value
-// get element by attribute name and value and tag
-// get element by attribute name and value and tag and class
+const addul = document.createElement('ul');
+const addli1 = document.createElement('li');
+const addli2 = document.createElement('li');
+const addli3 = document.createElement('li');
+
+addli1.textContent = 'one';
+addli2.textContent = 'two';
+addli3.textContent = 'three';
+
+addul.appendChild(addli1);
+addul.appendChild(addli2);
+addul.appendChild(addli3);
+
+document.body.appendChild(addul);
+const addanotherp = document.createElement('p');
+addanotherp.textContent = 'I am another p tag';
+addanotherp.style.color = 'red';
+addanotherp.style.fontSize = '50px';
+addanotherp.style.fontFamily = 'sans-serif';
+addanotherp.style.fontWeight = 'bold';
+addanotherp.id = 'newptag';
+addanotherp.className = 'newpclass';
+console.log(addanotherp);
+document.body.appendChild(addanotherp);
