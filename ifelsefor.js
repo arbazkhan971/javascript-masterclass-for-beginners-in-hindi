ee2480a6548e43268d6e24cdb3f06ccebc4709b9
arbaz <arbazkhan971@gmail.com>
2023-06-18 13:00:56 +0530

    added changes

---
+++ ifelsefor.js
@@ -0,0 +1,47 @@
+
+const name = "zaidi";
+if(name === "zaid"){
+    console.log("name is zaid")
+}else{
+    console.log("name is not zaid")
+}
+
+const age = 18;
+if(age > 18){
+    console.log("age is greater than 18")
+}else if(age === 18){
+    console.log("age is equal to 18")
+}
+else{
+    console.log("age is less than 18")
+}
+
+// const a = int(prompt("Enter a number"));
+// const b = int(prompt("Enter a number"));
+// const c = int(prompt("Enter a number"));
+// if(a > b && a > c){
+//     console.log("a is greater")
+// }
+// else if(b > a && b > c){
+//     console.log("b is greater")
+// }
+// else if(c > a && c > b){
+//     console.log("c is greater")
+// }
+// else{
+//     console.log("all are equal")
+// }
+
+const a = 10;
+const b = 20;
+const c = 30;
+
+if( a > b && a > c){
+    console.log("a is greater")
+}else if( b> a && b > c){
+    console.log("b is greater")
+}else if( c > a && c > b){
+    console.log("c is greater")
+}else{
+    console.log("all are equal")
+}
