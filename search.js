38b74cddd524994b82d32025ddc02a5d8efecad8
arbaz <arbazkhan971@gmail.com>
2023-07-02 20:13:50 +0530

    added hidden search

---
+++ search.js
@@ -0,0 +1,8 @@
+const search = document.querySelector('.search');
+const searchInput = document.querySelector('.input');
+const searchBtn = document.querySelector('.btn');
+
+searchBtn.addEventListener('click', () => {
+    search.classList.toggle('active');
+    // searchInput.focus();
+});
