f9b60eca2402316d9cdd5c4e61d899e286259fab
arbaz <arbazkhan971@gmail.com>
2023-12-31 19:46:47 +0530

    added changes

--- scrollanimation1.js
+++ scrollanimation1.js
@@ -1,14 +1,16 @@
 const scrollbar = document.querySelector("#scroll-bar")
 
 window.addEventListener("DOMContentLoaded",()=>{
-
     window.addEventListener("scroll",()=>{
         const scrollY = window.scrollY;
         const overallHeight = document.body.scrollHeight
         const innerHeight = window.innerHeight
+        console.log(window.screenY)
+        console.log(document.body.scrollHeight)
+        console.log(window.innerHeight)
+        console.log(document.body.scrollHeight-window.innerHeight)
+        console.log(scrollY/(overallHeight-innerHeight))
         const percentage = scrollY/(overallHeight-innerHeight)
         scrollbar.style.width = `${percentage*100}%`
     })
-
-
 })
